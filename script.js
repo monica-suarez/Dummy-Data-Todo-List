10166659ff638861459b11b61967c0f945d8b3f7
monica-suarez <shutterbugmo@gmail.com>
2020-03-30 21:03:56 -0500

    created for loop

---
+++ script.js
@@ -0,0 +1,33 @@
+// We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
+
+let arrayOfTodos = [
+    {
+    "userId": 14,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 20,
+    "id": 2,
+    "title": "delectus aut autem",
+    "completed": false
+  }]
+
+  const fetchTodos = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then( (response) => response.json())
+    .then( (json) => arrayOfTodos = json)
+  }
+
+  const logTodos = () => {
+    console.log(arrayOfTodos[0].title)
+  }
+
+  const populateTodos = () => {
+    for (let i = 0; i <= length.arrayOfTodos; i++){
+    const element = document.createElement('li');
+    element.innerHTML = arrayOfTodos[i].title;
+    document.getElementById("todo-list").appendChild(element);
+  }  
+}
