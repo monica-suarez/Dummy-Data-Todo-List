3dd4bd2b2883250b2336e6c4178802a4388deb0e
monica-suarez <shutterbugmo@gmail.com>
2020-03-30 21:24:35 -0500

    got it to work

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
 
-let arrayOfTodos = [
-    {
-    "userId": 14,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-  },
-  {
-    "userId": 20,
-    "id": 2,
-    "title": "delectus aut autem",
-    "completed": false
-  }]
-
+let arrayOfTodos = []
+const list = document.getElementById("todo-list")
+console.log(list)
   const fetchTodos = () => {
     fetch('https://jsonplaceholder.typicode.com/todos')
     .then( (response) => response.json())
@@ -21,13 +10,13 @@ let arrayOfTodos = [
   }
 
   const logTodos = () => {
-    console.log(arrayOfTodos[0].title)
+    console.log()
   }
 
   const populateTodos = () => {
-    for (let i = 0; i <= length.arrayOfTodos; i++){
-    const element = document.createElement('li');
-    element.innerHTML = arrayOfTodos[i].title;
-    document.getElementById("todo-list").appendChild(element);
+    for (let i = 0; i <= arrayOfTodos.length; i++){
+    const element = document.createElement("li");
+    element.appendChild(document.createTextNode(arrayOfTodos[i].title));
+    list.appendChild(element);
   }  
 }
