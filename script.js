42b890d45da1e241a3206e433eeaa1422a3647ce
monica-suarez <shutterbugmo@gmail.com>
2020-04-05 15:52:39 -0500

    finished this abomination

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
 
 let arrayOfTodos = []
+let filteredArray = []
+let completedArray = []
+let incompletedArray = []
 const list = document.getElementById("todo-list")
 console.log(list)
   const fetchTodos = () => {
@@ -25,17 +28,39 @@ console.log(list)
     list.appendChild(element)
   }  
 }
+let userInput = 0
 const todosById = () =>{
-    for (let i = 0; i < arrayOfTodos.length; i++){
-    const filteredElement = document.createElement("li");
-    filteredElement.appendChild(document.createTextNode(arrayOfTodos[i].filter(array => array.userId).title));
-  }
-  console.log
-  list.appendChild(filteredElement)
+  list.innerHTML = ""
+  userInput = Number(document.getElementById("user-id").value)
+  filteredArray = arrayOfTodos.filter(array => array.userId == userInput)
+  for (let i = 0; i < filteredArray.length; i++){
+    const element = document.createElement("li");
+    element.appendChild(document.createTextNode(filteredArray[i].title));
+    if(arrayOfTodos[i].completed == false){
+      element.classList.add("redColor");
+    }else{
+      element.classList.add("purpleColor");
+    }
+    list.appendChild(element)
+}
 }
 const completedTodos = () =>{
-
+  list.innerHTML = ""
+  completedArray = arrayOfTodos.filter(array => array.completed == true)
+  for (let i = 0; i < completedArray.length; i++){
+    const element = document.createElement("li");
+    element.appendChild(document.createTextNode(completedArray[i].title)); 
+    element.classList.add("purpleColor");
+    list.appendChild(element)
+}
 }
 const incompletedTodos = () => {
-
+  list.innerHTML = ""
+  incompletedArray = arrayOfTodos.filter(array => array.completed == false)
+  for (let i = 0; i < incompletedArray.length; i++){
+    const element = document.createElement("li");
+    element.appendChild(document.createTextNode(incompletedArray[i].title)); 
+    element.classList.add("redColor");
+    list.appendChild(element)
+}
 }
